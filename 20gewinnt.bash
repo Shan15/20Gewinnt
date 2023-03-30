3d91dd2de44e1483069d3c144a9adec30a0a3d09
FabioKaelin <supermae206@gmail.com>
2023-03-30 12:26:14 +0200

    added more functions

--- 20gewinnt.bash
+++ 20gewinnt.bash
@@ -7,6 +7,10 @@
 # Der Spieler, der als die Zahl 20 erreicht, hat gewonnen.
 
 currentValue=0
+
+# Required
+# gameMode = "pvp" or "pve" or "eve"
+
 # read -p "Multiplayer(m) oder Singleplayer(s)? " gameMode
 
 # if [ $gameMode = "m" ]; then
@@ -18,14 +22,23 @@ currentValue=0
 # fi
 
 printWinner(){
-    echo "Glückwunsch $currentPlayer, du hast gewonnen!"
+    # check if player or bot won
+    if [ $currentPlayerType = "player" ]; then
+        echo -e "\033[32mGlückwunsch $currentPlayer, du hast gewonnen!\033[0m"
+    else
+        echo -e "\033[31mDer Bot hat gewonnen!\033[0m"
+    fi
     exit 0
 }
 
-read -p "wie heißt du? " playerName
+checkEnd(){
+    if [ $currentValue -ge 20 ]; then
+        printWinner
+    fi
+}
 
-while true; do
-    currentPlayer="$playerName"
+getUserInput(){
+    currentPlayerType="player"
     read -p "Aktuller Stand is $currentValue - $playerName, wie viel möchtest du hinzufügen (1 oder 2)? " playerInput
     # Validate playerinput
     while [ $playerInput -ne 1 ] && [ $playerInput -ne 2 ]; do
@@ -34,21 +47,24 @@ while true; do
     done
 
     currentValue=$((currentValue + playerInput))
+}
 
-    # Check if currentValue is bigger or equal to 20
-    if [ $currentValue -ge 20 ]; then
-        printWinner
-    fi
-
-    # Bots turn
-    currentPlayer="Bot"
+getBotInput(){
+    currentPlayerType="bot"
     botInput=$((RANDOM % 2 + 1))
     echo "Aktuller Stand is $currentValue - Der Bot wird $botInput hinzufügen"
     currentValue=$((currentValue + botInput))
+}
 
-    if [ $currentValue -ge 20 ]; then
-        printWinner
-    fi
+read -p "wie heißt du? " playerName
 
+while true; do
+    currentPlayer="$playerName"
+    getUserInput
+    checkEnd
+
+    currentPlayer="Bot"
+    getBotInput
+    checkEnd
 done
 
